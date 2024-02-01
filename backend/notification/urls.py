def4fd16b2176ce2ba297551c026bdc0a53178f4
sfnicholas <75962325+sfnicholas@users.noreply.github.com>
2024-02-01 04:39:45 -0500

    Delete backend directory

--- backend/notification/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('notifications', NotificationListView.as_view(), name='notification-list'),
-    path('notifications/<int:pk>', NotificationUpdateDeleteView.as_view(), name='notification-update'),
-    path('notifications/view', NotificationView.as_view(), name='get-notification'),
-]
--- backend/petlisting/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PetListingCreate, PetListingRetrieveUpdateDestroy, PetListingList
-
-urlpatterns = [
-    path('petlistings', PetListingList.as_view(), name='petlisting-list'),  # List and search pet listings
-    path('petlistings/new', PetListingCreate.as_view(), name='petlisting-create'),  # Create a new pet listing
-    path('petlistings/<int:pk>', PetListingRetrieveUpdateDestroy.as_view(), name='petlisting-detail'),  # Retrieve, update, or delete a pet listing
-]
