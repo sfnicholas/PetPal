bdd2afc5a58d913b637982a50e5ca9ded9d6864c
sfnicholas <75962325+sfnicholas@users.noreply.github.com>
2024-02-01 04:21:48 -0500

    Delete backend directory

--- backend/application/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Application
-
-class ApplicationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Application
-        fields = ['application_id', 'pet_listing', 'applicant', 'status', 'creation_time', 'last_update_time']
-        read_only_fields = ['application_id', 'pet_listing', 'applicant', 'creation_time', 'last_update_time']
